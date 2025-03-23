c23920e3584aca1f0db92722fa3d476f16c13bfd
yifanma <yifanma@node0.joint-learning.wisr-pg0.utah.cloudlab.us>
2025-03-22 21:47:32 -0600

    Added .gitignore, README; removed blank CSV files.

--- computation_server.py
+++ computation_server.py
@@ -58,12 +58,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# Build new server structure with mm-link parameters:
-# UPLINK = wired300
-# DOWNLINK = wired12
-
-# Commands
-# RECV: python3 receiver.py --host=0.0.0.0 --port=7000
-# COMM: python3 communication_server.py --recv_host=0.0.0.0 --recv_port=6000 --forward_host=10.0.0.2 --forward_port=7000
-# COMP: python3 computation_server.py --comm_host=127.0.0.1 --comm_port=6000 --num_jobs=5 --matrix_size=3
